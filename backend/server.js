9c1bb49066188b35a476fe324ffc50c7a86fef2a
Andre Palhares <apalhares@ecisolutions.com>
2025-02-16 19:26:53 +0000

    fixing minor bug

--- backend/server.js
+++ backend/server.js
@@ -12,9 +12,9 @@ const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 app.use(express.json());
+app.use("/api/products", productRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  console.log("production", process.env.NODE_ENV);
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
   app.get("*", (req, res) => {
@@ -22,7 +22,6 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-app.use("/api/products", productRoutes);
 app.listen(PORT, () => {
   connectDb();
   console.log("server started at http://localhost:" + PORT);
